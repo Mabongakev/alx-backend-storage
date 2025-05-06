acfe058bc66e95443559dc1d6a79122f7e273c04
Mabongakev <kelvin.mabonga92@gmail.com>
2025-05-06 22:39:41 +0300

    Task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python3
-""" 11-main """
-from pymongo import MongoClient
-list_all = __import__('8-all').list_all
-insert_school = __import__('9-insert_school').insert_school
-schools_by_topic = __import__('11-schools_by_topic').schools_by_topic
+"""
+Changes all topics of a schooll document based on the name
+Prototype: def update_topics(mongo_collection, name, topics):
+mongo_collection will be the pymongo object
+name (String) will be the school name to update
+topics (list of strings) will be the list of topics approached in the school
+"""
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
 
-    j_schools = [
-        {'name': "Holberton school", 'topics': [
-            "Algo", "C", "Python", "React"]},
-        {'name': "UCSF", 'topics': ["Algo", "MongoDB"]},
-        {'name': "UCLA", 'topics': ["C", "Python"]},
-        {'name': "UCSD", 'topics': ["Cassandra"]},
-        {'name': "Stanford", 'topics': ["C", "React", "Javascript"]}
-    ]
-    for j_school in j_schools:
-        insert_school(school_collection, **j_school)
-
-    schools = schools_by_topic(school_collection, "Python")
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'),
-              school.get('name'), school.get('topics', "")))
+def update_topics(mongo_collection, name, topics):
+    """
+    Prototype: def update_topics(mongo_collection, name, topics):
+    Changes all topics of a school document
+    """
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
